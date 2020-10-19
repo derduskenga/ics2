93e06d219b42fa5fd1af3d89b3f0d8c4299961cf
Kerdus Kenga <derduskenga@gmail.com>
2020-10-19 10:14:38 +0300

    account interface and class use added

---
+++ user.php
@@ -0,0 +1,17 @@
+<?php
+    class User implements Account{
+        public function register ($pdo) {
+
+        }
+        public function login($pdo) {
+
+        }
+        public function changePassword($pdo) {
+
+        }
+        public function logout ($pdo){
+
+        }
+    } 
+
+?>
